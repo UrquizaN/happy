c424809c284cbab07ba6dd4fdd44c436ca746470
UrquizaN <urquiza-nobrega@outlook.com>
2020-10-16 07:36:08 -0300

    feat: added create orphanage database

---
+++ backend/src/server.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
+
+import './database/connection';
+
+const server = express();
+
+server.use(express.json());
+
+server.post('/orphanages', async (request, response) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage)
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    })
+    
+    await orphanagesRepository.save(orphanage);
+
+    return response.status(201).json({ orphanage });
+})
+
+server.listen(3333);
